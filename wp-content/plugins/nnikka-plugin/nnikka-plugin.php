96bc3393c380278b05170acc7323cc25c73b2838
nnikka <nikakhurashvili@gmail.com>
2018-08-29 12:19:27 +0400

    plugin configuration

---
+++ wp-content/plugins/nnikka-plugin/nnikka-plugin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package NnikkaPlugin
+ */
+/*
+Plugin Name: Nnikka Plugin
+Plugin URI: http://nnikka.com
+Description: This is my first custom plugin
+Version: 1.0.0
+Author: Nikoloz Kurashvili
+Author URI: hhtp://nnikka.com
+License: GPLv2 or later
+Text Domain: nnikka-plugin
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
+*/
